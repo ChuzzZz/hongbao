b70b994f4efa9b822912154f4bfaac5e4e4fc097
Chuz <Dell@DESKTOP-F574IR5>
2017-08-11 11:06:15 +0800

    修正entity showinfo

--- src/main/java/entity/ShowInfo.java
+++ src/main/java/entity/ShowInfo.java
@@ -8,23 +8,23 @@ public class ShowInfo {
 	String show_name;
 	String performer;
 	String department;
-	Timestamp time;
+	Timestamp start_time;
 	
 	public ShowInfo() {}
-	public ShowInfo(int id, int s_order, String show_name, String performer, String department, Timestamp time) {
+	public ShowInfo(int id, int s_order, String show_name, String performer, String department, Timestamp start_time) {
 		super();
 		this.id = id;
 		this.s_order = s_order;
 		this.show_name = show_name;
 		this.performer = performer;
 		this.department = department;
-		this.time = time;
+		this.start_time = start_time;
 	}
 	
 	@Override
 	public String toString() {
 		return "ShowInfo [id=" + id + ", s_order=" + s_order + ", show_name=" + show_name + ", performer=" + performer
-				+ ", department=" + department + ", start_time=" + time + "]";
+				+ ", department=" + department + ", start_time=" + start_time + "]";
 	}
 	
 	public int getId() {
@@ -57,11 +57,11 @@ public class ShowInfo {
 	public void setDepartment(String department) {
 		this.department = department;
 	}
-	public Timestamp gettime() {
-		return time;
+	public Timestamp getStart_time() {
+		return start_time;
 	}
-	public void settime(Timestamp start_time) {
-		this.time = start_time;
+	public void setStart_time(Timestamp start_time) {
+		this.start_time = start_time;
 	}
 	
 }
