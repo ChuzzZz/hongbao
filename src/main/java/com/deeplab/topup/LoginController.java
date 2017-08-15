3252a0109aa86cd1d3c0b455abe67a7a29a9750f
TotPil <846685686@qq.com>
2017-08-15 10:12:52 +0800

    管理员界面

--- src/main/java/com/deeplab/topup/LoginController.java
+++ src/main/java/com/deeplab/topup/LoginController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import dao.LuckyMoneyDAO;
 import dao.UserDAO;
 
 @Controller
@@ -25,6 +26,11 @@ public class LoginController {
 		System.out.println(ic);
 		System.out.println(username);
 		if (ic == 10086 && username.equals("π‹¿Ì‘±")) {
+			if (LuckyMoneyDAO.getTotalByRound(1, jdbcTemplate)!=0){model.addAttribute("round1",0);}
+			if (LuckyMoneyDAO.getTotalByRound(2, jdbcTemplate)!=0){model.addAttribute("round2",0);}
+			if (LuckyMoneyDAO.getTotalByRound(3, jdbcTemplate)!=0){model.addAttribute("round3",0);}
+			long k = LuckyMoneyDAO.getTotalByRound(1, jdbcTemplate);
+			System.out.println(k);
 			return "administrator";
 		} else {
 			if (UserDAO.userExists(ic, username, jdbcTemplate)) {
